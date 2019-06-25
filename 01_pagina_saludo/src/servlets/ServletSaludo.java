9eec78f8e3214fde3e30b79bba388844c7ffaccd
ALUMNO <ALUMNO@Aula-Pc>
2019-06-25 09:23:10 +0200

    version2

--- 01_pagina_saludo/src/servlets/ServletSaludo.java
+++ 01_pagina_saludo/src/servlets/ServletSaludo.java
@@ -20,7 +20,7 @@ public class ServletSaludo extends HttpServlet {
 		out.println("<html>");
 		out.println("<body><center>");
 		for(int i=1;i<=6;i++) {
-			out.println("<h"+i+">Bienvenido a mi servlet</h"+i+">");
+			out.println("<h"+i+">Bienvenido a mi servlet otra vez</h"+i+">");
 		}
 		
 		out.println("</center></body>");
