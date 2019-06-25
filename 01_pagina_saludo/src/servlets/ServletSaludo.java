25a5986c608bfc5238812e411ac90f9d347cd985
ALUMNO <ALUMNO@Aula-Pc>
2019-06-25 09:19:05 +0200

    version1

---
+++ 01_pagina_saludo/src/servlets/ServletSaludo.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ServletSaludo")
+public class ServletSaludo extends HttpServlet {
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<html>");
+		out.println("<body><center>");
+		for(int i=1;i<=6;i++) {
+			out.println("<h"+i+">Bienvenido a mi servlet</h"+i+">");
+		}
+		
+		out.println("</center></body>");
+		out.println("</html>");
+		out.close();
+	}
+
+}
